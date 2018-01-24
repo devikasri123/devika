887018934e8a64aac61521c504685b54c981f542
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-01-24 16:51:53 +0530

    ninteger.c

---
+++ ninteger.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 9;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+        
+      printf("Number of digits: %d", count);
+}

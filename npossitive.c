3a2fd585466dd3c9b5c5b411cc58c851ea493677
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-01-24 16:20:58 +0530

    possitive.c

---
+++ npossitive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 4;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+   
+
+   
+    sum=sum+i;
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}

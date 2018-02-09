9c21d537f40138685f74cf51ef3a7f1432018ab6
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-02-09 16:17:37 +0530

    char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    char    str[100];
+    int countSpecialChar;
+    int counter;
+ 
+    
+    countSpecialChar=0;
+ 
+    printf("Enter a string: ");
+    printf(" \nSpecial Characters: %d");
+ 
+    return 0;
+}

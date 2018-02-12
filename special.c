8d20c01161dce0628221b1428d4ab7cc53314044
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-02-12 16:16:17 +0530

    special.c

---
+++ special.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main()
+{
+    char    str[100];
+    int countSpecialChar;
+    int counter;
+ 
+    countSpecialChar=5;
+ 
+    printf("Enter a string: ");
+    
+    {
+ 
+        if(str[counter]>='0' && str[counter]<='9')
+           
+        
+        
+            countSpecialChar++;
+    }
+ 
+    printf("\nSpecial Characters: %d",countSpecialChar);
+ 
+    return 0;
+}

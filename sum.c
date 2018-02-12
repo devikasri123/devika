e1d967a40c967878c22310b7fda7e9194ee85c86
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-02-12 16:45:07 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main()
+{
+   int i,j, k;
+ 
+   printf("Enter two numbers to add\n");
+   scanf("%d%d", &i, &j);
+ 
+   k = i + j;
+ 
+   printf("Sum of the numbers = %d\n", k);
+ 
+   return 0;
+}

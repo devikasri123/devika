4bdc58ea0c358f2d6fce053f639b0a33e48dab7c
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-02-21 16:27:24 +0530

    max10.c

---
+++ max10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+  int main()
+  {
+    int a[10];
+    int x;
+    int greatest;
+    printf("Enter ten values:\n");
+    for (x = 0; x < 10; x++)
+    {
+    scanf("%d", &a[x]);
+    }
+    greatest = a[0];
+    for (x = 0; x< 10; x++)
+    {
+if (a[x] > greatest)
+{
+greatest = a[x];
+    }
+    }
+    printf("Greatest of ten numbers is %d", greatest);
+    return 0;
+  }

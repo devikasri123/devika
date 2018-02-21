a144b8973c7441347d1387618311246c3e941dc9
devika.b <30549765+devikasri123@users.noreply.github.com>
2018-02-21 15:57:31 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+void main()
+{
+  int i,n,Sum=0,numbers;
+  float Average;
+ 
+  
+ 
+  printf("\nPlease Enter the elements one to one\n");
+  for(i=0;i<n;++i)
+   {
+     scanf("%d",&numbers);
+   }
+ 
+  Average = Sum/n;
+ 
+  printf("\nAverage of the %d Numbers = %.2f",n, Average);
+ 
+  return 0;
+}
